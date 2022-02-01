d5fbefce6217daa76080a5c74db6e6ea8a6ac248
Jose AL <neojal@hotmail.com>
2022-01-31 21:20:45 -0600

    2014 d04 p1 & p2. Pure brute force.

--- src/main/java/year2015/Day04.java
+++ src/main/java/year2015/Day04.java
@@ -1,35 +1,18 @@
 package year2015;
 
-import javax.xml.bind.DatatypeConverter;
-import java.security.MessageDigest;
+import helpers.AoCTransformData;
+
 import java.security.NoSuchAlgorithmException;
 
 public class Day04 {
-    public int md5Generator(String secret) throws NoSuchAlgorithmException {
-        boolean result;
 
-        int n = 0;
-
+    public int getNumberWithCondition(String secret, String condition) throws NoSuchAlgorithmException {
+        int n = -1;
+        String hash;
         do {
-            result = getPart1(String.format("%s%d", secret, n++));
-        } while (!result);
-
-        return n - 1;
-    }
-
-    private boolean getPart1(String secret) throws NoSuchAlgorithmException {
-        String hash = getMD5HashFromSecret(secret);
-        if (hash.startsWith("00000")) {
-            return true;
-        }
-        return false;
-    }
-
-    public String getMD5HashFromSecret(String secret) throws NoSuchAlgorithmException {
-
-        MessageDigest md = MessageDigest.getInstance("MD5");
-        md.update(secret.getBytes());
-        byte[] digest = md.digest();
-        return DatatypeConverter.printHexBinary(digest).toLowerCase();
+            String newSecret = secret.concat(Integer.toString(++n));
+            hash = AoCTransformData.getMD5HashFromSecret(newSecret);
+        } while (!hash.startsWith(condition));
+        return n;
     }
 }
